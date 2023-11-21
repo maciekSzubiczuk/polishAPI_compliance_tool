710a2efc0d2e25554c0484b182c5d4350ae817e3
Maciej Szubiczuk <maciej.szubiczuk1@gmail.com>
2023-11-21 11:32:52 +0100

    added simple comparison for pis

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import yaml
+from deepdiff import DeepDiff
 
 app = Flask(__name__)
 
@@ -7,20 +8,37 @@ app = Flask(__name__)
 polish_api_data = None
 santander_api_data = []
 
+def load_yaml_from_file(file):
+    if file:
+        return yaml.safe_load(file)
+
+def compare_apis(polish_api, santander_api):
+    differences = DeepDiff(polish_api, santander_api, ignore_order=True)
+    return json_serializable_diff(differences.to_dict())
+
+def json_serializable_diff(diff_dict):
+    """ Recursively convert complex objects in diff_dict to serializable formats. """
+    if isinstance(diff_dict, dict):
+        for key, value in diff_dict.items():
+            diff_dict[key] = json_serializable_diff(value)
+    elif not isinstance(diff_dict, (int, float, str, list, dict, bool, type(None))):
+        return str(diff_dict)  # Convert non-serializable types to string
+    return diff_dict
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     global polish_api_data, santander_api_data
     if request.method == 'POST':
-        # Handle Polish API file upload
+        # Handle file uploads
         polish_api_file = request.files.get('polishapi_file')
-        if polish_api_file and polish_api_file.filename.endswith('.yaml'):
-            polish_api_data = yaml.safe_load(polish_api_file)
-
-        # Handle Santander files upload
         santander_files = request.files.getlist('santander_files')
+
+        if polish_api_file:
+            polish_api_data = load_yaml_from_file(polish_api_file)
+
         for file in santander_files:
-            if file and file.filename.endswith('.yaml'):
-                santander_api_data.append(yaml.safe_load(file))
+            if file:
+                santander_api_data.append(load_yaml_from_file(file))
 
         return redirect(url_for('display'))
 
@@ -28,13 +46,18 @@ def index():
 
 @app.route('/display')
 def display():
-    # Convert YAML data to string for display
-    polish_api_str = yaml.dump(polish_api_data, default_flow_style=False)
-    santander_api_str = [yaml.dump(data, default_flow_style=False) for data in santander_api_data]
-    return render_template('display.html', polish_api=polish_api_str, santander_api=santander_api_str)
+    # Compare the APIs and get differences
+    differences = []
+    for santander_api in santander_api_data:
+        diff = compare_apis(polish_api_data, santander_api)
+        differences.append(diff)
+
+    # Render display page with differences
+    return render_template('display.html', differences=differences)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
